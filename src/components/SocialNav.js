e56006bd847f160d14babc02e2213f3abb3521f5
Onitwin <onitwin@gmail.com>
2021-10-27 10:37:52 +0100

    banner image complete

--- src/components/SocialNav.js
+++ src/components/SocialNav.js
@@ -3,9 +3,9 @@ import React from "react";
 const SocialNav = () => {
   return (
     <div className="social-icon-wrap">
-      <img className="social-icon" src="/svg/facebook.svg" alt="facebook" />;
-      <img className="social-icon" src="/svg/twitter.svg" alt="facebook" />;
-      <img className="social-icon" src="/svg/insta.svg" alt="facebook" />;
+      <img className="social-icon" src="/svg/facebook.svg" alt="facebook" />
+      <img className="social-icon" src="/svg/twitter.svg" alt="facebook" />
+      <img className="social-icon" src="/svg/insta.svg" alt="facebook" />
     </div>
   );
 };
